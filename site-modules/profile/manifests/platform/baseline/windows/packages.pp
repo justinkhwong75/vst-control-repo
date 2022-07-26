dddd67938fa3f53834d42df7250ce0f8ffa9ba8a
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-26 15:59:43 +0800

    Update packages.pp

--- site-modules/profile/manifests/platform/baseline/windows/packages.pp
+++ site-modules/profile/manifests/platform/baseline/windows/packages.pp
@@ -9,21 +9,21 @@ class profile::platform::baseline::windows::packages {
     command => 'msiexec /i C:\Temp\Agent-Windows-20.0.0-4959.x86_64\Agent-Core-Windows-20.0.0-4959.x86_64.msi /qn /norestart /l* C:\Temp\Agent-Windows-20.0.0-4959.x86_64\install.log',
   }
 
-  package { 'notepadplusplus':
-      ensure            => installed,
-      provider          => 'chocolatey',
-  }
+  #package { 'notepadplusplus':
+  #    ensure            => installed,
+  #    provider          => 'chocolatey',
+  #}
   
-  package { '7zip':
-      ensure            => installed,
-      provider          => 'chocolatey',
-  }
+  #package { '7zip':
+  #    ensure            => installed,
+  #    provider          => 'chocolatey',
+  #}
   
-  package { 'kav':
-      ensure            => installed,
-      provider          => 'chocolatey',
-      install_options => ['-override', '-installArgs', '"', '/VERYSILENT', '/NORESTART', '"'],
-  }
+  #package { 'kav':
+  #    ensure            => installed,
+  #    provider          => 'chocolatey',
+  #    install_options => ['-override', '-installArgs', '"', '/VERYSILENT', '/NORESTART', '"'],
+  #}
   #$predefined_packages = [ 'notepadplusplus', '7zip', 'git', 'uniextract' ]
   #package { $predefined_packages:
   #  ensure => present
