b6fb555f2d01898c3ad0e85845123ab889afeb68
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-26 16:19:42 +0800

    Update packages.pp

--- site-modules/profile/manifests/platform/baseline/windows/packages.pp
+++ site-modules/profile/manifests/platform/baseline/windows/packages.pp
@@ -6,7 +6,7 @@ class profile::platform::baseline::windows::packages {
   #}
 
   exec { 'trend_micro':
-    command => 'msiexec /i C:\Temp\Agent-Windows-20.0.0-4959.x86_64\Agent-Core-Windows-20.0.0-4959.x86_64.msi /qn /norestart /l* C:\Temp\Agent-Windows-20.0.0-4959.x86_64\install.log',
+    command => 'C:\Windows\System32\msiexec.exe /i C:\Temp\Agent-Windows-20.0.0-4959.x86_64\Agent-Core-Windows-20.0.0-4959.x86_64.msi /qn /norestart /l* C:\Temp\Agent-Windows-20.0.0-4959.x86_64\install.log',
   }
 
   #package { 'notepadplusplus':
