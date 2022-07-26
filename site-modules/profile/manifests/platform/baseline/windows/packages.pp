079c9b94e31f728d84c0e3a55e9bfaf64c7a2056
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-26 16:24:00 +0800

    Update packages.pp

--- site-modules/profile/manifests/platform/baseline/windows/packages.pp
+++ site-modules/profile/manifests/platform/baseline/windows/packages.pp
@@ -1,40 +1,7 @@
 class profile::platform::baseline::windows::packages {
 
-  include chocolatey
-  #Package {
-  #  provider => chocolatey,
-  #}
-
   exec { 'trend_micro':
     command => 'C:\Windows\System32\msiexec.exe /i C:\Temp\Agent-Windows-20.0.0-4959.x86_64\Agent-Core-Windows-20.0.0-4959.x86_64.msi /qn /norestart /l* C:\Temp\Agent-Windows-20.0.0-4959.x86_64\install.log',
   }
 
-  #package { 'notepadplusplus':
-  #    ensure            => installed,
-  #    provider          => 'chocolatey',
-  #}
-  
-  #package { '7zip':
-  #    ensure            => installed,
-  #    provider          => 'chocolatey',
-  #}
-  
-  #package { 'kav':
-  #    ensure            => installed,
-  #    provider          => 'chocolatey',
-  #    install_options => ['-override', '-installArgs', '"', '/VERYSILENT', '/NORESTART', '"'],
-  #}
-  #$predefined_packages = [ 'notepadplusplus', '7zip', 'git', 'uniextract' ]
-  #package { $predefined_packages:
-  #  ensure => present
-  #}
-
-  #unless getvar('trusted.external.servicenow.u_enforced_packages').empty {
-  #  $packages = parsejson($trusted['external']['servicenow']['u_enforced_packages'])
-  #  $packages.each |$package,$ensure|{
-  #    package { $package:
-  #      ensure => $ensure
-  #    }
-  #  }
-  #}
 }
