ae0010516250c0e010b685adb3772fcf19715e96
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-27 10:29:56 +0800

    Update mssql.pp

--- site-modules/profile/manifests/app/mssql.pp
+++ site-modules/profile/manifests/app/mssql.pp
@@ -1,10 +1,10 @@
-class 
+class profile::app::mssql { 
 
+include chocolatey
 
-package { ‘GoogleChrome’:
-    ensure          => ‘100.0.4896.88’, # Updated 11th Apr 2022
-    provider        => ‘chocolatey’,
+  package { ‘sql-server-express’:
+    ensure          => installed
     install_options => [‘-y’],
-    source          => ‘internal_chocolatey’,
-    require         => Package[‘chocolatey.extension’],
   }
+  
+}
