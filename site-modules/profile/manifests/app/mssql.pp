328fe48d6616f83fbcc8fcd03f0992d607fc6daa
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-27 16:23:55 +0800

    Update mssql.pp

--- site-modules/profile/manifests/app/mssql.pp
+++ site-modules/profile/manifests/app/mssql.pp
@@ -2,9 +2,12 @@ class profile::app::mssql {
 
 include chocolatey
 
-  package {'sql-server-express':
-    ensure => installed,
-    provider => 'chocolatey',
-    install_options => ['-y'],
+  exec {
   }
+  
+  #package {'sql-server-express':
+  #  ensure => installed,
+  #  provider => 'chocolatey',
+  #  install_options => ['-y'],
+  #}
 }
