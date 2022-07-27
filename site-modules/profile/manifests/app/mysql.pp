d13156b2e4b41abbfdd7fe0541db47f8dc082c6d
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-27 19:55:42 +0800

    Update mysql.pp

--- site-modules/profile/manifests/app/mysql.pp
+++ site-modules/profile/manifests/app/mysql.pp
@@ -1,10 +1,6 @@
 class profile::app::mysql {
 
 include chocolatey
-
-  #exec { 'mssql':
-  #  command => 'C:\ProgramData\chocolatey\bin\choco.exe install sql-server-express -y -f',
-  #}
   
   package {'mysql':
     ensure => installed,
