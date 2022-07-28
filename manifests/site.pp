5b328b9aff8dc4a67e17dc34e47a141db7ffa11c
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-07-28 15:36:19 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,14 +4,14 @@
 File { backup => false }
 
 node default {
-  include profile::platform::baseline
+  #include profile::platform::baseline
   
   # Check if we've set the role for this node via trusted fact, pp_role.  If yes; include that role directly here.
-  if $trusted['extensions']['pp_role'] {
-    include $trusted['extensions']['pp_role']
-  } else {
-    notify { "No such role\n": }
-  }
+  #if $trusted['extensions']['pp_role'] {
+  #  include $trusted['extensions']['pp_role']
+  #} else {
+  #  notify { "No such role\n": }
+  #}
 }
 
 # Uncomment the line below to enable ServiceNow Node Classification
