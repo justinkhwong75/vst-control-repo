980e8cf2be304cde59334caee120f3a2376ef484
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-08-29 11:10:44 +0800

    Create autosign.pp

---
+++ site-modules/profile/manifests/platform/autosign.pp
@@ -0,0 +1,23 @@
+class profile::platform::autosign {
+
+  ini_setting { 'policy-based autosigning':
+    setting => 'autosign',
+    path    => "${confdir}/puppet.conf",
+    section => 'master',
+    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+    notify  => Service['pe-puppetserver'],
+  }
+
+  class { ::autosign:
+    ensure => 'latest',
+    config => {
+      'general' => {
+        'loglevel' => 'INFO',
+      },
+      'jwt_token' => {
+        'secret'   => 'hunter2'
+        'validity' => '7200',
+      }
+    },
+  }
+}
